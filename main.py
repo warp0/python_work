ca994a6f5b3dc6b933b6eddc4dfbebf4fc75f213
warp0_home <fdeepreg@gmail.com>
2019-08-08 22:18:58 +0300

    final version with all features except UI

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import requests
 import sqlite3
 import re
 import pickle
-import sqlalchemy
+from collections import Counter 
+import json
 
 #defining classes
 class tags:
-    number = str
     timestamp = str
     url = str
     dictionary = str
@@ -29,7 +29,6 @@ def initDB():
     if proceed == "Yes":
         cursor.execute("""CREATE TABLE tagcounter(
             URL         TEXT    NOT NULL,
-            TAGS        INT     NOT NULL,
             TIMESTAMP   TEXT    NOT NULL,
             DICTIONARY  BLOB    NOT NULL
         );"""
@@ -41,21 +40,26 @@ def initDB():
 
 def recordDB(tags):
     #query
-    queries.tagcounterGet="""INSERT INTO 'tagcounter'
-    (URL, TAGS, TIMESTAMP, DICTIONARY) VALUES (?, ?, ?, ?);"""
+    queries.tagcounterPut="""INSERT INTO 'tagcounter'
+    (URL, TIMESTAMP, DICTIONARY) VALUES (?, ?, ?);"""
     db_dictionary=pickle.dumps(tags.dictionary, protocol=pickle.HIGHEST_PROTOCOL)
-    binTags=(tags.url, tags.number, tags.timestamp, db_dictionary)
-    cursor.execute(queries.tagcounterGet, binTags)
+    binTags=(tags.url, tags.timestamp, db_dictionary)
+    cursor.execute(queries.tagcounterPut, binTags)
     dbconnect.commit()
+
+def getquery(url):
+    queries.tagcounterGet="""SELECT * FROM 'tagcounter' WHERE URL=?"""
+    for row in cursor.execute(queries.tagcounterGet, (url,)):
+        print(row[0:2], pickle.loads(row[2]))
 
 #counting tags number on page: CONSUMING URL, RETURNING TAGS OBJECT
 def getcount(url):
     response=requests.get(url)
 
-    tags.number = response.text.count("<")
     tags.timestamp = timestamp.strftime("%m/%d/%Y, %H:%M:%S")
     tags.url = url
 
+    #working with dictionary
     dictionary=re.findall('<\s*[a-z, A-Z]*[\s>]', response.text)
     tags.dictionary=[]
     for i in dictionary:
@@ -66,12 +70,17 @@ def getcount(url):
             tags.dictionary.append(i)
         else: 
             print("found empty tag")
-    tags.dictionary=list(dict.fromkeys(tags.dictionary))
+    tags.dictionary=Counter(tags.dictionary)
+    
+    #logging
+    log_obj = {
+        'time': tags.timestamp,
+        'url' : tags.url
+    }
+    with open('./parsed.log','a') as logfile:
+        json.dump(log_obj, logfile)
+        logfile.write("\n")
 
-def getQuery(url):
-    queries.tagcounterPut="""SELECT * FROM tagcounter where URL=?"""
-    for row in cursor.execute(queries.tagcounterPut, (url,)):
-        print(row[0:3], pickle.loads(row[3]))
 
 #getting args and defining url
 url=""
@@ -79,49 +88,58 @@ args_number=len(sys.argv)
 
 if sys.argv[1]=="--get":
     initDB()
-    for i in range(2,args_number):
-
-        timestamp=datetime.now()
-        url=sys.argv[i]
-
-        if url=="ggl": 
-            getcount("https://www.google.com")
-
-        elif url=="msn": 
-            getcount("https://www.msn.com")
-
-        elif url[0:1]=="-":
+    for index1 in range(2,args_number):
+        #check for correct syntax
+        if url[0:1]=="-":
             print("Err:too many commands or wrong argument expression!")
             break
-
-        else: 
+        
+        #reset alias_checker
+        alias_checker=0
+        
+        #check for time and set url from args
+        timestamp=datetime.now()
+        url=sys.argv[index1]
+        
+        #read aliases
+        with open("./aliases.cfg", "r") as alias_file:
+            alias_map=json.load(alias_file)
+        for alias in alias_map:  
+            if alias==url:
+                getcount(alias_map[alias])
+                alias_checker=alias_checker + 1
+                print("alias found")
+        
+        #if alias check failed - we have valid url:
+        if alias_checker < 1:
             getcount(url)
          
-        #print objects
-        print(tags.url, tags.number, tags.timestamp)
+        #print obtained data
+        print(tags.url, tags.timestamp)
         print(tags.dictionary)
         print(len(tags.dictionary))
-
+        
+        #record object to database
         recordDB(tags)
 
 elif sys.argv[1]=="--view":
-    
-    for i in range(2,args_number):
-       
-        url=sys.argv[i]
+    for index2 in range(2,args_number):
+        alias_checker=0
+        url=sys.argv[index2]
+        
+        with open("./aliases.cfg", "r") as alias_file:
+            alias_map=json.load(alias_file)
+        
+        for alias in alias_map:  
+            if alias==url:
+                getquery(alias_map[alias])
+                alias_checker=alias_checker + 1
+                print("alias found")
 
-        if url=="ggl": 
-            getQuery("https://www.google.com")
-
-        elif url=="msn": 
-            getQuery("https://www.msn.com")
-
-        elif url[0:1]=="-":
-            print("Err:too many commands or wrong argument expression!")
-            break
-        else:
-            getQuery(url)
-
+        #if alias check not incremented - we have non-alias url:
+        if alias_checker < 1:
+            getquery(url)
+        
 
 #if no --command found                
 else: 
